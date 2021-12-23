2e1b3dfd4b6379fbc17fd349aa0778f2db02b9e6
huyphu2005 <96584712+huyphu2005@users.noreply.github.com>
2021-12-23 22:10:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: ':)))',
+    desc: ':))) ',
+    btnYes: 'đẹp vãi lon',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
