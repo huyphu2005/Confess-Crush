b738cf98c727c9f4e6106e6cc88eb43529887b1b
huyphu2005 <96584712+huyphu2005@users.noreply.github.com>
2021-12-23 22:09:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "yeyeyeyey ",
-    introTitle: 'trang',
+    titleWeb: "boss có đẹp trai ko ",
+    introTitle: 'boss có đẹp trai ko',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
